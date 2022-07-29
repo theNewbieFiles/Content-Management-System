16a1f7b47fb86d074968eeef50dc7c019260171a
Chris Wilson <chris.wilson@chrispcr.com>
2022-07-28 22:42:02 -0600

    Example config.php file.
    This is an example config file that the system is looking for, but without the info, like my password.

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+ini_set( "display_errors", true );
+date_default_timezone_set( "America/Denver" );// http://www.php.net/manual/en/timezones.php
+
+
+//file caching
+define("FIlE_CACHING", true);
+
+
+
+define( "HOST", "" );
+define( "DB_NAME", "" );
+define( "DB_USERNAME", "" );
+define( "DB_PASSWORD", "" );
+
+//Other settings as needed. 
+
+
